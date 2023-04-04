1e50a3ccbdce4b28b8b61a8bc3a19a93773b2cb3
Mateus Chines <mateuschines>
2023-04-03 22:58:56 -0300

    Aula 03/04

---
+++ src/pages/Carrinho/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Menu } from '../../components/Menu'
+import { Button, TBTr, THTh, THtr, Table, Td, TextButton } from './styles'
+
+export const Carrinho = () => {
+    return (
+        <>
+            <Menu />
+            <div
+                style={{
+                    paddingLeft: '6%',
+                    paddingRight: '6%',
+                    marginTop: 20,
+                    marginBottom: 40
+                }}
+            >
+                <h2>Carrinho de compras</h2>
+
+                <Table>
+                    <thead>
+                        <THtr>
+                            <THTh
+                                style={{
+                                    minWidth: 300
+                                }}
+                            >Nome do Produto</THTh>
+                            <THTh>Quantidade</THTh>
+                            <THTh>Vlr Unit.</THTh>
+                            <THTh>Vlr Total.</THTh>
+                            <THTh>Ações</THTh>
+                        </THtr>
+                    </thead>
+                    <tbody>
+                        <TBTr>
+                            <Td>Mouse</Td>
+                            <Td>1</Td>
+                            <Td>1,99</Td>
+                            <Td>1,99</Td>
+                            <Td>
+                                <Button
+                                    type='button'
+                                >
+                                    <TextButton>
+                                        Lixo
+                                    </TextButton>
+                                </Button>
+                            </Td>
+                        </TBTr>
+                    </tbody>
+                </Table>
+            </div>
+        </>
+    )
+}
