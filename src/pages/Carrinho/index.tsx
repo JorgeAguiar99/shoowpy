3e09462b21de040a674b29f345d65486f67aaf61
Mateus Chines <mateuschines>
2023-04-04 22:16:12 -0300

    Aula dia 04/04

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Menu } from '../../components/Menu'
+import { FaTrash } from 'react-icons/fa'
 import { Button, TBTr, THTh, THtr, Table, Td, TextButton } from './styles'
 
+interface interfProdutos {
+    id_categoria: number
+    total: number
+    id: string
+    imagemg: string
+    imagemp: string
+    nome: string
+    promo: string
+    promoNumber: string
+    quantidade: string
+    valor: string
+}
+
 export const Carrinho = () => {
+
+    const [dataCarrinho, setDataCarrinho] = useState<Array<interfProdutos>>([])
+    const [valorTotal, setValorTotal] = useState<number>(0)
+
+    const atualizaValorTotal = useCallback(
+        (carrinho: Array<interfProdutos>) => {
+        let total = 0
+
+        carrinho.forEach((produto) => {
+            total = produto.total + total
+        })
+        setValorTotal(total)
+
+    },[])
+    // function atualizaValorTotal(
+    //     carrinho: Array<interfProdutos>
+    // ) {
+    //     let total = 0
+
+
+    //     carrinho.forEach((produto) => {
+    //         total = produto.total + total
+    //     })
+    //     setValorTotal(total)
+
+    //     console.log(valorTotal)
+
+    // }
+
+    useEffect(() => {
+        let lsCarrinho = localStorage.getItem(
+            '@shoowpy:carrinho'
+        )
+        let carrinho: any = null
+
+        if (typeof lsCarrinho === 'string') {
+            carrinho = JSON.parse(lsCarrinho)
+        }
+
+        if (carrinho) {
+            setDataCarrinho(carrinho)
+            atualizaValorTotal(carrinho)
+        }
+
+    }, [])
+
+    function removerProdutoCarrinho(id: string) {
+        let carrinho = dataCarrinho.filter((produto) => (
+            produto.id !== id
+        ))
+
+        localStorage.setItem(
+            '@shoowpy:carrinho',
+            JSON.stringify(carrinho)
+        )
+
+        setDataCarrinho(carrinho)
+        atualizaValorTotal(carrinho)
+    }
+
     return (
         <>
             <Menu />
@@ -31,23 +105,65 @@ export const Carrinho = () => {
                         </THtr>
                     </thead>
                     <tbody>
-                        <TBTr>
-                            <Td>Mouse</Td>
-                            <Td>1</Td>
-                            <Td>1,99</Td>
-                            <Td>1,99</Td>
-                            <Td>
-                                <Button
-                                    type='button'
-                                >
-                                    <TextButton>
-                                        Lixo
-                                    </TextButton>
-                                </Button>
-                            </Td>
-                        </TBTr>
+
+                        {
+                            dataCarrinho.map((produto) => {
+                                return (
+                                    <TBTr key={produto.id}>
+                                        <Td width={300}>{produto.nome}</Td>
+                                        <Td>{produto.quantidade}</Td>
+                                        <Td>{produto.promo}</Td>
+                                        <Td>{produto.total}</Td>
+                                        <Td>
+                                            <Button
+                                                type='button'
+                                                onClick={() => {
+                                                    removerProdutoCarrinho(produto.id)
+                                                }}
+                                            >
+                                                <TextButton>
+                                                    <FaTrash />
+                                                </TextButton>
+                                            </Button>
+                                        </Td>
+                                    </TBTr>
+                                )
+                            })
+                        }
+
                     </tbody>
+                    <tfoot>
+                        <TBTr>
+                            <Td width={300}>Valor Total</Td>
+                            <Td></Td>
+                            <Td></Td>
+                            <Td>{valorTotal}</Td>
+                            <Td></Td>
+                        </TBTr>
+                    </tfoot>
                 </Table>
+
+                {
+                    dataCarrinho.length > 0 &&
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <Button
+                            type='button'
+                        >
+                            <TextButton>Limpar carrinho</TextButton>
+                        </Button>
+                        <Button
+                            type='button'
+                            bgColor='green'
+                        >
+                            <TextButton>Finalizar pedido</TextButton>
+                        </Button>
+                    </div>
+                }
             </div>
         </>
     )
